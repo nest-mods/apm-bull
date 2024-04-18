45ef07295b197210f984e6067c1e9b6371da4b13
Diluka W <pmars42@gmail.com>
2024-04-18 17:02:58 +0800

    refactor: setHandler

--- index.js
+++ index.js
@@ -3,34 +3,26 @@ const apm = require('elastic-apm-node/start');
 const shimmer = require('elastic-apm-node/lib/instrumentation/shimmer');
 const Queue = require('bull/lib/queue');
 
-shimmer.wrap(Queue.prototype, 'process', function (original) {
-    return function process() {
-        const args = Array.from(arguments);
-
-        for (let i = 0; i < args.length; i++) {
-            const _handler = args[i];
-
-            // only handle function handler
-            if (typeof _handler === 'function') {
-                args[i] = async function (job) {
-                    const transName = `${job.queue.name}[${job.name}]`;
-                    const trans = apm.startTransaction(transName, 'queue');
-                    apm.setCustomContext(job.toJSON());
-                    try {
-                        return await _handler(job);
-                    } catch (e) {
-                        trans.result = e.message;
-                        trans.outcome = 'failure';
-                        apm.captureError(e);
-                        throw e;
-                    } finally {
-                        trans.end();
-                    }
-                };
-                break;
-            }
+shimmer.wrap(Queue.prototype, 'setHandler', function (original) {
+    return function setHandler(name, handler) {
+        if (typeof handler === 'function') {
+            const _handler = handler;
+            handler = async function (job) {
+                const transName = `${job.queue.name}[${job.name}]`;
+                const trans = apm.startTransaction(transName, 'queue');
+                apm.setCustomContext(job.toJSON());
+                try {
+                    return await _handler(job);
+                } catch (e) {
+                    trans.result = e.message;
+                    trans.outcome = 'failure';
+                    apm.captureError(e);
+                    throw e;
+                } finally {
+                    trans.end();
+                }
+            };
         }
-
-        return original.apply(this, args);
-    };
+        return original.call(this, name, handler);
+    }
 });
