9fa336aaea36ad3b07934372dd899eb4900b0332
Diluka W <pmars42@gmail.com>
2024-05-24 16:04:31 +0800

    Revert "fix(bullmq): await processFn"
    This reverts commit a7ac33ea3433e06d56444365ad6a189ca1fe9f60.

--- bullmq.js
+++ bullmq.js
@@ -4,13 +4,13 @@ const shimmer = require('elastic-apm-node/lib/instrumentation/shimmer');
 const { Worker } = require('bullmq');
 
 shimmer.wrap(Worker.prototype, 'callProcessJob', function (original) {
-    return async function callProcessJob(job, token) {
+    return function callProcessJob(job, token) {
         const transName = `${job.queueName}[${job.name}]`;
         const trans = apm.startTransaction(transName, 'queue');
         apm.setCustomContext(job.toJSON());
         try {
             trans.outcome = 'success';
-            return await original.call(this, job, token);
+            return original.call(this, job, token);
         } catch (e) {
             trans.result = e.message;
             trans.outcome = 'failure';
