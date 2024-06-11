00deac772184cd71916c6c7abf70978d94205ef7
liaozijian <lzj960515@163.com>
2024-06-11 15:33:49 +0800

    feat: pre resolve

--- bullmq.js
+++ bullmq.js
@@ -4,33 +4,21 @@ const shimmer = require('elastic-apm-node/lib/instrumentation/shimmer');
 const {Worker} = require('bullmq');
 
 shimmer.wrap(Worker.prototype, 'callProcessJob', function (original) {
-    return function callProcessJob(job, token) {
+    return async function callProcessJob(job, token) {
+        await Promise.resolve();
         const transName = `${job.queueName}[${job.name}]`;
         const trans = apm.startTransaction(transName, 'queue');
         apm.setCustomContext(job.toJSON());
         try {
             trans.outcome = 'success';
-            const result = original.call(this, job, token);
-            if (result && typeof result.catch === 'function' && typeof result.finally === 'function') {
-                // is async handler
-                result.catch((e) => {
-                    trans.result = e.message;
-                    trans.outcome = 'failure';
-                    apm.captureError(e);
-                    throw e;
-                }).finally(() => {
-                    trans.end();
-                });
-            } else {
-                trans.end();
-            }
-            return result;
+            return await original.call(this, job, token);
         } catch (e) {
             trans.result = e.message;
             trans.outcome = 'failure';
             apm.captureError(e);
-            trans.end();
             throw e;
+        } finally {
+            trans.end();
         }
     };
 });
